e8037eff37facf309601b96af282acef92f58920
Rishon73 <sshiff@gmail.com>
2018-09-05 16:16:02 -0400

    minor changes

--- src/main/java/com/mf/MyHttpClientFactory.java
+++ src/main/java/com/mf/MyHttpClientFactory.java
@@ -1,9 +1,9 @@
 package com.mf;
 
-import java.net.URL;
-
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.openqa.selenium.remote.internal.ApacheHttpClient;
+
+import java.net.URL;
 
 public class MyHttpClientFactory implements org.openqa.selenium.remote.http.HttpClient.Factory {
     final HttpClientBuilder builder;
